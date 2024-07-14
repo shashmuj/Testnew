a93e043adb7b587bc86514997cc301aa00c95c0a
Shashwat Mujumdar <shashwatbunny@gmail.com>
2024-07-14 13:41:31 +0100

    change

--- Receivercode.py
+++ Receivercode.py
@@ -15,7 +15,7 @@ class MyCustomHeader(Packet):
         ByteField("ttl", 64),                       # Time To Live (TTL)
         ByteField("protocol", 253),                 # Protocol number (custom protocol number)
         XShortField("checksum", 0),                 # Checksum (initially set to 0, will be calculated later)
-        IPField("src", "94.119.93.116"),          # Source IP address
+        IPField("src", "167.98.155.199"),          # Source IP address
         IPField("dst", "128.110.217.192")           # Destination IP address
     ]
 
@@ -72,7 +72,7 @@ def handle_packet(packet):
 def main():
     """Main function to start packet sniffing."""
     print("Starting packet sniffing...")
-    sniff(filter="ip and src host 94.119.93.116", iface="eno1", prn=handle_packet)
+    sniff(filter="ip and src host 167.98.155.199", iface="eno1", prn=handle_packet)
 
 if __name__ == "__main__":
     main()
